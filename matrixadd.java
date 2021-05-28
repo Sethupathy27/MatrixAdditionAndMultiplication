90c1bcb067e903c1bba94655b02c179eef8e0d23
Sethupathy27 <83457122+Sethupathy27@users.noreply.github.com>
2021-05-28 21:15:50 +0530

    matrixadd.java

---
+++ matrixadd.java
@@ -0,0 +1,30 @@
+package matrixaddition;
+
+/**
+ *
+ * @author sethu
+ */
+public class MatrixAddition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here   
+        int a[][]={{1,2,3},{4,5,6},{7,8,9}};    
+        int b[][]={{1,3,4},{2,4,3},{1,2,4}};    
+       //creating another matrix to store the sum of two matrices    
+     int c[][]=new int[3][3];  //3 rows and 3 columns  
+        //adding and printing addition of 2 matrices    
+     for(int i=0;i<3;i++)
+     {
+        for(int j=0;j<3;j++)
+        {
+         c[i][j]=a[i][j]+b[i][j];    //use - for subtraction  
+         System.out.print(c[i][j]+" ");    
+        }    
+      System.out.println();//new line    
+      }    
+    }
+    
+}
